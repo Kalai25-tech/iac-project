d9cd9c7a46d2e4c1454701ae4955c4dab7765f97
Kalai25-tech <kalaiyarasan788@gmail.com>
2025-03-29 04:19:54 +0530

    flow7

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,10 @@
-provider "aws" {
-  region = var.region
-}
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
+
+provider "aws" {
+  region = var.region
 }
 
 data "aws_availability_zones" "available" {}
@@ -13,30 +13,4 @@ locals {
   cluster_name = var.clusterName
 }
 
-resource "aws_cloudwatch_log_group" "eks_log_group" {
-  name = "/aws/eks/${local.cluster_name}/cluster"
-
-  retention_in_days = 30  # Adjust as needed
-
-  lifecycle {
-    ignore_changes = [name]  # Prevent unnecessary changes
-  }
-}
-
-# Import existing CloudWatch log group if it already exists
-resource "null_resource" "import_log_group" {
-  provisioner "local-exec" {
-    command = <<EOT
-      if aws logs describe-log-groups --log-group-name-prefix "/aws/eks/${local.cluster_name}/cluster" --region ${var.region} | grep logGroupName; then
-        echo "Log Group Exists, Importing..."
-        terraform import aws_cloudwatch_log_group.eks_log_group "/aws/eks/${local.cluster_name}/cluster" || echo "Already Managed"
-      else
-        echo "Log Group Does Not Exist, Creating..."
-      fi
-    EOT
-  }
-
-  triggers = {
-    always_run = "${timestamp()}"
-  }
-}
+##
